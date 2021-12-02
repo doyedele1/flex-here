1621e21bd1cb54d183322035b33a9036d11f02f5
doyedele1 <demilade.oyedele@gmail.com>
2021-12-02 18:29:32 +0100

    Added house images to public folder

---
+++ pages/houses.js
@@ -0,0 +1,15 @@
+housesArray = [
+    {
+        picture: '/img/1.jpg',
+        type: 'Entire house',
+        town: 'New York',
+        title: 'Beautiful flat in New York!'
+    },
+    {
+        picture: 'img/2.jpg',
+        type: 'Entire house',
+        town: 'Amsterdam',
+        title: 'A flat in Amsterdam with a great view'
+    }
+]
+export default housesArray;
