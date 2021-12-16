7543e63b0c0b57c5f245e4dcc039dbac7d26a93b
doyedele1 <demilade.oyedele@gmail.com>
2021-12-16 17:55:31 +0100

    Completed the detail page of the houses

--- pages/houses/[id].js
+++ pages/houses/[id].js
@@ -1,20 +1,28 @@
 /* eslint-disable @next/next/no-img-element */
+import Head from 'next/head';
 import houses from '../../houses';
 
-const House = (props) => {
-    <div>
-        <img src={props.house.picture} width="100%" alt="House" />
-        <p>{props.house.type} - {props.house.town}</p>
-        <p>{props.house.title}</p>
-    </div>
+export default function House(props) {
+    return (
+        <div>
+            <Head>
+                <title>{props.house.title}</title>
+            </Head>
+            <img src={props.house.picture} width="100%" alt="House picture" />
+            <p>
+                {props.house.type} - {props.house.town}
+            </p>
+            <p>{props.house.title}</p>
+        </div>
+    )
 }
 
-House.getInitialProps = ({ query}) => {
+export async function getServerSideProps({ query }) {
     const { id } = query;
 
     return {
-        house: houses.filter((house) => house.id === id)[0]
+        props: {
+            house: houses.filter((house) => house.id === parseInt(id))[0]
+        }
     }
 }
-
-export default House;
