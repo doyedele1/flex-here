8d6c4e9585b34dc95c4d83cf9a9be985d685d61c
doyedele1 <demilade.oyedele@gmail.com>
2022-03-05 09:08:08 +0100

    Feat: Connected to Railway PostgreSQL DB and implemented the user model for signup

---
+++ model.js
@@ -0,0 +1,47 @@
+import { Sequelize, Model, DataTypes } from 'sequelize';
+import bcrypt from 'bcrypt';
+
+const sequelize = new Sequelize(process.env.POSTGRES_CONNECTION, {
+    dialect: 'postgres',
+    logging: false,
+});
+
+class User extends Model {};
+
+User.init(
+    {
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        session_token: {
+            type: DataTypes.STRING
+        },
+        session_expiration: {
+            type: DataTypes.DATEONLY
+        }
+    },
+    {
+        sequelize,
+        modelName: 'user',
+        timestamps: false,
+        hooks: {
+            beforeCreate: async (user) => {
+                const saltRounds = 10;
+                const salt = await bcrypt.genSalt(saltRounds);
+                user.password = await bcrypt.hash(user.password, salt);
+            }
+        }
+    }
+);
+
+User.prototype.isPasswordValid = async function(password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+export { sequelize, User };
+
--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,9 +1,29 @@
-// eslint-disable-next-line import/no-anonymous-default-export
-export default (req, res) => {
+/* eslint-disable import/no-anonymous-default-export */
+import { User } from '../../../model.js';
+
+export default async (req, res) => {
     if (req.method !== 'POST') {
         res.status(405).end();
         return;
     }
-    console.log(req.body);
-    res.end();
+    
+    const { email, password, passwordConfirmation } = req.body;
+
+    if (password !== passwordConfirmation) {
+        res.end(
+            JSON.stringify({
+                status: 'error',
+                message: 'Passwords do not match'
+            })
+        );
+        return;
+    }
+
+    const user = await User.create({ email, password });
+    res.end(
+        JSON.stringify({
+            status: 'success',
+            message: 'User created'
+        })
+    );
 }
